09492d353f671692cf789e3f68569b3b5e8d8b9d
aryan <aryananand005@gmail.com>
2025-05-28 17:06:21 +0530

    contact

--- lib/UI/contact.dart
+++ lib/UI/contact.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:email_validator/email_validator.dart';
 
@@ -13,7 +14,7 @@ class _ContactMeState extends State<ContactMe> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _messageController = TextEditingController();
 
-  Future<void> _sendMessage() async {
+  Future<void> _checkField() async {
     final email = _emailController.text.trim();
     final message = _messageController.text.trim();
 
@@ -24,37 +25,27 @@ class _ContactMeState extends State<ContactMe> {
       return;
     }
 
-    if( !EmailValidator.validate(email)) {
+    if (!EmailValidator.validate(email)) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Please enter a valid email address.')),
       );
       return;
     }
 
-    final Uri emailUri = Uri(
-      scheme: 'mailto',
-      path: 'aryananand005@gmail.com',
-      queryParameters: {
-        'subject': 'Contact from $email',
-        'body': message,
-      },
-    );
-
-    await launchUrl(emailUri).then((success) {
-      if (!success) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Could not open email app.')),
-        );
-      }
-    }).catchError((error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error: $error')),
-      );
-    });
-
+    _sendEmail(email, message);
 
     _emailController.clear();
     _messageController.clear();
+  }
+
+  void _sendEmail(String email, String message) async {
+    final Email sender = Email(
+      body: message,
+      subject: 'Contact from $email',
+      recipients: ['aryananand005@gmail.com'],
+    );
+
+    await FlutterEmailSender.send(sender);
   }
 
   @override
@@ -62,84 +53,89 @@ class _ContactMeState extends State<ContactMe> {
     return Scaffold(
         backgroundColor: Colors.black,
         body: SingleChildScrollView(
-          child: Padding(padding: EdgeInsets.all(16), child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 24, bottom: 16),
-                child: Align(
-                  alignment: Alignment.centerLeft, // Aligns text to the left
-                  child: Text(
-                    "Contact Me",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 24, bottom: 16),
+                  child: Align(
+                    alignment: Alignment.centerLeft, // Aligns text to the left
+                    child: Text(
+                      "Contact Me",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-
-              SizedBox(height: 30,),
-              TextField(
-                controller: _emailController,
-                style: const TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Sender\'s Address',
-                  labelStyle: const TextStyle(color: Colors.white70),
-                  alignLabelWithHint: true,
-                  filled: true,
-                  fillColor: Colors.grey[850],
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(12),
+                SizedBox(
+                  height: 30,
+                ),
+                TextField(
+                  controller: _emailController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Sender\'s Address',
+                    labelStyle: const TextStyle(color: Colors.white70),
+                    alignLabelWithHint: true,
+                    filled: true,
+                    fillColor: Colors.grey[850],
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.white, width: 2),
-                    borderRadius: BorderRadius.circular(12),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 40),
+                TextField(
+                  controller: _messageController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Your Message',
+                    alignLabelWithHint: true,
+                    labelStyle: const TextStyle(color: Colors.white70),
+                    filled: true,
+                    fillColor: Colors.grey[850],
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  maxLines: 6,
+                  keyboardType: TextInputType.multiline,
+                ),
+                const SizedBox(height: 24),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton.icon(
+                    onPressed: _checkField,
+                    icon: const Icon(Icons.send, color: Colors.black),
+                    label: const Text("Send",
+                        style: TextStyle(color: Colors.black)),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                    ),
                   ),
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 40),
-              TextField(
-                controller: _messageController,
-                style: const TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Your Message',
-                  alignLabelWithHint: true,
-                  labelStyle: const TextStyle(color: Colors.white70),
-                  filled: true,
-                  fillColor: Colors.grey[850],
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.white, width: 2),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                maxLines: 6,
-                keyboardType: TextInputType.multiline,
-              ),
-              const SizedBox(height: 24),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton.icon(
-                  onPressed: _sendMessage,
-                  icon: const Icon(Icons.send, color: Colors.black),
-                  label: const Text(
-                      "Send", style: TextStyle(color: Colors.black)),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 12),
-                  ),
-                ),
-              ),
-            ],
-          ),),
-        )
-    );
+              ],
+            ),
+          ),
+        ));
   }
 }
